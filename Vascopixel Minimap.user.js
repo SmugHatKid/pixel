8101fff957b60767fd2503f88bf0bab90176842e
SmugHatKid <47949123+SmugHatKid@users.noreply.github.com>
2019-02-25 16:26:55 +0300

    Update Vascopixel Minimap.user.js

--- Vascopixel Minimap.user.js
+++ Vascopixel Minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/SmugHatKid/pixel/master';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/SmugHatKid/pixel/master/';
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
     re = /(.*)\/\?p=(\-?(?:\d*)),(\-?(?:\d*))/g;
